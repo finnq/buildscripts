fd11af14d396c10c477ec4ebaad88682433693ac
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-02-05 21:46:43 +0100

    allow to set build type

--- build.sh
+++ build.sh
@@ -247,6 +247,18 @@ fi
 # Starting Timer
 START=$(date +%s)
 
+case "$EXTRACMD" in
+    eng)
+		BUILD_TYPE=eng
+		;;
+    userdebug)
+		BUILD_TYPE=userdebug
+		;;
+    *)
+		BUILD_TYPE=eng
+		;;
+esac
+
 # Device specific settings
 case "$CMD" in
     prepare)
@@ -264,7 +276,7 @@ case "$CMD" in
         exit
         ;;
     *)
-        lunch=cm_${CMD}-userdebug
+        lunch=cm_${CMD}-${BUILD_TYPE}
         brunch=${lunch}
         ;;
 esac
